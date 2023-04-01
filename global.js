e570f29a69809181b9016bc82cfe7a4b18249916
Keiv Holding <holding.keiv@gmail.com>
2023-04-01 19:42:15 +0200

    access token

--- global.js
+++ global.js
@@ -1,3 +1,31 @@
+function getTokensFromUrl() {
+  const hashParams = new URLSearchParams(window.location.hash.substring(1));
+  const accessToken = hashParams.get("access_token");
+  const refreshToken = hashParams.get("refresh_token");
+
+  if (accessToken && refreshToken) {
+    console.log("Access Token:", accessToken);
+    console.log("Refresh Token:", refreshToken);
+    return accessToken;
+  } else {
+    console.error("Access token or refresh token is missing in the URL.");
+  }
+}
+
+const accessTokenFunc = getTokensFromUrl();
+console.log(accessTokenFunc);
+
+const url = "https://salesmachine.vercel.app/api/getCampaigns";
+fetch(url, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({ accessToken }),
+})
+  .then((response) => response.json())
+  .then((data) => console.log({ data }));
+
 //
 //
 // CREATING AN ACCOUNT
--- my-campaigns.js
+++ my-campaigns.js
@@ -1,31 +1,3 @@
-function getTokensFromUrl() {
-  const hashParams = new URLSearchParams(window.location.hash.substring(1));
-  const accessToken = hashParams.get("access_token");
-  const refreshToken = hashParams.get("refresh_token");
-
-  if (accessToken && refreshToken) {
-    console.log("Access Token:", accessToken);
-    console.log("Refresh Token:", refreshToken);
-  } else {
-    console.error("Access token or refresh token is missing in the URL.");
-  }
-}
-
-getTokensFromUrl();
-
-console.log(accessToken);
-
-const url = "https://salesmachine.vercel.app/api/getCampaigns";
-fetch(url, {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({ accessToken }),
-})
-  .then((response) => response.json())
-  .then((data) => console.log({ data }));
-
 const campaigns = {
   data: [
     {
