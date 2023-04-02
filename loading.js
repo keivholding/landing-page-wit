8c19d985f982246d641f6db634136a059d520682
Keiv Holding <holding.keiv@gmail.com>
2023-04-02 16:00:15 +0200

    test

--- loading.js
+++ loading.js
@@ -13,13 +13,11 @@ loading.style.zIndex = "9999";
 
 // this makes it so that the loading / progress bar is shown when the page is loading - then hidden when the page is fully loaded
 window.addEventListener("load", function () {
-  setTimeout(() => {
-    // adds the class show so that it fades out
-    loading.classList.add("hide");
+  // adds the class show so that it fades out
+  loading.classList.add("hide");
 
-    // when the opacity transition is finished, it gets moved to the back with z-index
-    setTimeout(() => {
-      loading.style.zIndex = "-9999";
-    }, 400);
-  }, 0);
+  // when the opacity transition is finished, it gets moved to the back with z-index
+  setTimeout(() => {
+    loading.style.zIndex = "-9999";
+  }, 400);
 });
